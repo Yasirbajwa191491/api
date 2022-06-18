eb4724aed77e8f3374b0daef75646872e7e7c01e
Yasirsoha <yasirsohail191491@gmail.com>
2022-06-18 10:56:25 -0700

    pushed changed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
     <>
     <Navbar />
     <Routes>
-      <Route path="/ApiWithRedux" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path='/add' element={<Add />} />
       <Route path='/about' element={<About />} />
       <Route path='/contact' element={<Contact />} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,14 +6,14 @@ const Navbar = () => {
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container">
-    <NavLink className="navbar-brand" to="/ApiWithRedux" style={{fontSize:"15px"}}>Yasir191491</NavLink>
+    <NavLink className="navbar-brand" to="/" style={{fontSize:"15px"}}>Yasir191491</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <NavLink className="nav-link" aria-current="page" to="/ApiWithRedux" style={{fontSize:"15px"}}>Home</NavLink>
+          <NavLink className="nav-link" aria-current="page" to="/" style={{fontSize:"15px"}}>Home</NavLink>
         </li>
         <li className="nav-item">
           <NavLink className="nav-link" to="/add" style={{fontSize:"15px"}}>Add_New</NavLink>
